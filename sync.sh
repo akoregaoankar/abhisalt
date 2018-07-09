8ad54a8ce5ed2253b0632343895cae438faa5b88
root <abhikore4729@gmail.com>
2018-07-09 08:49:59 +0530

    change in sync.sh

--- sync.sh
+++ sync.sh
@@ -4,7 +4,7 @@ sudo cp master /etc/salt
 ## Remove old SLS
 rm -rvf /srv/salt/*
 ## Copy new SLS
-cp salt/* /srv/salt/
+cp -R salt/* /srv/salt/
 ## Restart services
 sudo /etc/init.d/salt-master restart
 sudo /etc/init.d/salt-master status
