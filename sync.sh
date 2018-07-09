a7e5d11d6e339560113c13b1cc9e949111d8bb6b
root <abhikore4729@gmail.com>
2018-07-09 08:33:41 +0530

    change in top.sls

--- sync.sh
+++ sync.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 ## copy salt master
 sudo cp master /etc/salt
-
+## Remove old SLS
+rm -rvf /srv/salt/*
+## Copy new SLS
+cp salt/* /srv/salt/
+## Restart services
 sudo /etc/init.d/salt-master restart
 sudo /etc/init.d/salt-master status
+
